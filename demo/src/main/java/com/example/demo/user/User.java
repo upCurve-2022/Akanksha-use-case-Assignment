e10f04d1dcbf7a988d98e03d1cacb42e0238a535
Akanksha Faujdar <67420568+akku131@users.noreply.github.com>
2022-06-27 13:29:13 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/user/User.java
@@ -0,0 +1,79 @@
+package com.example.demo.user;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false,unique = true ,length = 45)
+    private String email;
+    @Column(length = 15,nullable = false)
+    private String password;
+    @Column(length = 45,nullable = false,name="First_Name")
+    private String firstname;
+    @Column(length = 45,nullable = false,name="Last_Name")
+    private  String lastname;
+
+    private boolean enabled;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public  void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
---
+++ demo/src/test/java/com/example/demo/UserRepositoryTests.java
@@ -0,0 +1,67 @@
+package com.example.demo;
+
+import com.example.demo.user.User;
+import com.example.demo.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Optional;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+
+public class UserRepositoryTests {
+    @Autowired private UserRepository repo;
+    @Test
+    public void testAddNew(){
+        User user=new User();
+        user.setEmail("19cse009@gweca.ac.in");
+        user.setFirstname("Aditi");
+        user.setLastname("asdfg");
+        user.setPassword("akashbna90");
+        User savedUser=repo.save(user);
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testListAll(){
+        Iterable<User> users=repo.findAll();
+        Assertions.assertThat(users).hasSizeGreaterThan(0);
+        for (User user:users){
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testUpdate(){
+        Integer userId=1;
+        Optional<User> optionalUser=repo.findById(userId);
+        User user= optionalUser.get();
+        user.setPassword("hello1224");
+        repo.save(user);
+
+        User updatedUser=repo.findById(userId).get();
+        Assertions.assertThat(updatedUser.getPassword()).isEqualTo("hello1224");
+    }
+    @Test
+    public void testGet(){
+        Integer userId=3;
+        Optional<User> optionalUser=repo.findById(userId);
+        User user= optionalUser.get();
+        Assertions.assertThat(optionalUser).isPresent();
+        System.out.println(optionalUser.get());
+    }
+    @Test
+    public void testDelete(){
+        Integer userId=5;
+        repo.deleteById(userId);
+        Optional<User> optionalUser=repo.findById(userId);
+        Assertions.assertThat(optionalUser).isNotPresent();
+    }
+}
